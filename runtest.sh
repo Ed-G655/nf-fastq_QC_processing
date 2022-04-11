8af8ac09271d81840438eac169424064f6d7a558
Edu <jeduardogl655@gmail.com>
2022-04-11 17:50:49 -0500

    Add run with docker

--- runtest.sh
+++ runtest.sh
@@ -9,4 +9,5 @@ echo -e "======\n Testing NF execution \n======" \
 	-resume \
 	-with-report $output_directory/`date +%Y%m%d_%H%M%S`_report.html \
 	-with-dag $output_directory/`date +%Y%m%d_%H%M%S`.DAG.html \
+	-with-docker \
 && echo -e "======\n Basic pipeline TEST SUCCESSFUL \n======"
